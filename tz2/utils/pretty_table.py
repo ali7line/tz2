52ddf96e7ee7ba838c05bddfe3f62c9ce6bf87fc
ali7line <soltani.110@gmail.com>
2018-01-04 16:50:09 +0330

    fixes issue with not-latin characters

--- tz2/utils/pretty_table.py
+++ tz2/utils/pretty_table.py
@@ -1,10 +1,21 @@
 import click
+import re
+
+
+def remove_nonlatin(text):
+    return re.sub(r'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]', '', str(text))
 
 
 def pretty_table(table):
-    click.echo("{0:>4} | {1:^40} | {2:^5} | {3:^5} | {4:^10} | {5:^8} | {6:^6} | {7:^6}".format(
+    click.echo("{0:>4} | {1:^40} | {2:^25} | {3:^5} | {4:^10} | {5:^8} | {6:^6} | {7:^6}".format(
         'id', 'name', 'cat', 'verif', 'age', 'size', 'peers', 'leech'))
-    click.echo("-"*(4+40+5+5+10+8+6+6+21))
+    click.echo("-"*(4+40+5+5+10+8+6+6+21+20))
     for id_, name, cat, verif, age, size, peers, leech, links in table:
-        click.echo("{0:>4} | {1:^40} | {2:<5} | {3:^5} | {4:<10} | {5:<8} | {6:^6} | {7:^6}".format(
-            id_, name[:40], cat, verif, age, size, peers, leech))
+        new_cat = remove_nonlatin(cat)
+        new_name = remove_nonlatin(name)
+        if len(name) > 40:
+            click.echo("{0:>4} | {1:^40} | {2:<25} | {3:^5} | {4:<10} | {5:<8} | {6:^6} | {7:^6}".format(
+                id_, new_name[:40], new_cat, verif, age, size, peers, leech))
+        else:
+            click.echo("{0:>4} | {1:^40} | {2:<25} | {3:^5} | {4:<10} | {5:<8} | {6:^6} | {7:^6}".format(
+                id_, new_name, new_cat, verif, age, size, peers, leech))
